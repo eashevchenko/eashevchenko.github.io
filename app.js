c65642ed76d5f13f2442d52342fedf3669f75f3d
eshevchenko <eugene.shevchenko@swanlogic.com>
2017-01-20 00:32:25 +0200

    edited app js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-$.get('https://api.github.com/users/eashevchenko', function(data) {
+
+$( document ).ready(function() {
+   $.get('https://api.github.com/users/eashevchenko', function(data) {
 	$('#data').text(JSON.stringify(data));
+	});
 });
